249e90a3fe06850d908a3d4afd2ade60eebc2974
jisham <jishamvallikkadan@gmail.com>
2025-03-01 22:55:08 +0530

    loacal db Added

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           home: const OnBoarding(),
+
           // home:  ChapterListScreen(),
         ),
       ),
